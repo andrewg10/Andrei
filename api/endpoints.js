49705459ef8ef02c0a90c609121f86be703aa41c
Andrei <35070653+andrewg10@users.noreply.github.com>
2025-11-26 18:55:05 +0200

    Update endpoints.js

--- api/endpoints.js
+++ api/endpoints.js
@@ -11,6 +11,6 @@ function getCurrentWeatherEndpoint(city) {
 	return `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
 }
 
-function getForecatEndpoint(city) {
+function getForecastEndpoint(city) {
     return `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=ro&units=metric&appid=${API_KEY}`
 }
