77c4c8ca12df9a4eda8e1eea9fbec2285c57a8f1
Andrei <35070653+andrewg10@users.noreply.github.com>
2025-11-26 18:42:33 +0200

    Update endpoints.js

--- api/endpoints.js
+++ api/endpoints.js
@@ -1,4 +1,4 @@
-const API_KEY = "fc939fd77ea2d21ce7e9262f4f6cb604";
+const API_KEY = "9718e1f7e0c4672dd201bf2c5c53d13b";
 // API key-urile Nu ar trebui stocate in repo si in format text deoarece nu este secure. API key-urile de preferat e sa fie stoacate pe un server insa fiindca API-ul de la Open Weather este gratuit momentan e ok sa il tinem aici.
 
 // Definim endpoint-urile catre server:
