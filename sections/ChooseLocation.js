db8c08041f6bfd9f1492d7b6b1677a9924fd5e38
Andrei <35070653+andrewg10@users.noreply.github.com>
2025-11-26 18:34:50 +0200

    Update ChooseLocation.js

--- sections/ChooseLocation.js
+++ sections/ChooseLocation.js
@@ -5,6 +5,21 @@ const oradeaButton = document.querySelector('.dropdown-item.oradea');
 const arad = document.querySelector(".dropdown-menu .arad");
 const sibiu = document.querySelector(".dropdown-menu .sibiu");
 
+function updateCurrentCityName(city){
+    //selectam tag ul de html unde o sa updatam orasul curent
+    const currentCity=document.querySelector('.current-city');
+    currentCity.innerHTML=${city}.;
+}
+function updateWeather(cityName){
+    //salvam in local storage optiunea aleasa
+    localStorage.setItem('city',cityName);
+    //afisam vremea curenta
+    displayCurrentWeather(cityName);
+    //apelam functia care ne schimba orasul curent de pe ecran
+    updateCurrentCityName(cityName);
+    //reafisam si prognoza pentru urmatoarele 5 zile
+    displayWeatherForecast(cityName);
+}
 // Adaugam event linstere pe butoane pentru a schimba datele despre vreme
 bucharestButton.addEventListener('click', function(){
     console.log('clicked');
