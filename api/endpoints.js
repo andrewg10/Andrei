bcf3b391f1e74004cec4a889a3eaba8d6fb73c6a
Andrei <35070653+andrewg10@users.noreply.github.com>
2025-11-26 16:01:53 +0200

    Add files via upload

---
+++ api/endpoints.js
@@ -0,0 +1,16 @@
+const API_KEY = "fc939fd77ea2d21ce7e9262f4f6cb604";
+// API key-urile Nu ar trebui stocate in repo si in format text deoarece nu este secure. API key-urile de preferat e sa fie stoacate pe un server insa fiindca API-ul de la Open Weather este gratuit momentan e ok sa il tinem aici.
+
+// Definim endpoint-urile catre server:
+// Pentru endpoint de vreme curenta trebuie sa inseram dinamic parametrul city - ne definim in functie un parametru
+function getCurrentWeatherEndpoint(city) {
+	// API-ul de la Open Weather are nevoie de urmatorii query params:
+	// city
+	// lang - noi o sa trimitem ro
+	// units - metric, pentru a primit rezultatele in grade Celsius
+	return `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
+}
+
+function getForecatEndpoint(city) {
+    return `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=ro&units=metric&appid=${API_KEY}`
+}
